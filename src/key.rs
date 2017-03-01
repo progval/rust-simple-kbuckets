31b33bbb063a488f1bde05eedc7a75c946615397
Valentin Lorentz <progval+git@progval.net>
2017-03-01 18:50:30 +0100

    Remove useless method from Key trait.

--- src/key.rs
+++ src/key.rs
@@ -3,16 +3,12 @@ use std::fmt::Debug;
 /// Trait that table keys must implement.
 pub trait Key: Debug + Clone + Ord + PartialEq {
     fn bitxor(&self, other: &Self) -> Self;
-    fn is_zero(&self) -> bool;
     fn bits(&self) -> usize;
 }
 
 impl Key for u64 {
     fn bitxor(&self, other: &u64) -> u64 {
         self ^ other
-    }
-    fn is_zero(&self) -> bool {
-        *self == 0
     }
     fn bits(&self) -> usize {
         (64 - self.leading_zeros()) as usize
@@ -22,9 +18,6 @@ impl Key for u64 {
 impl Key for Vec<u8> {
     fn bitxor(&self, other: &Vec<u8>) -> Vec<u8> {
         self.iter().zip(other.iter()).map(|(digit1, digit2)| digit1 ^ digit2).collect()
-    }
-    fn is_zero(&self) -> bool {
-        self.iter().all(|digit| *digit == 0)
     }
     fn bits(&self) -> usize {
         let mut bits = self.len()*8;
